b037ce03db5c6861c7d5d397c4a4d9fd7bf3708c
ana <ana.sacramento@musicpro.live>
2025-01-20 14:13:31 -0300

    feat: Atualizações

--- src/infrastructure/utils/config.ts
+++ src/infrastructure/utils/config.ts
@@ -7,5 +7,19 @@ import { ChatController } from '../../interface/chat-controller';
 import { CreateChatUseCase } from '../../application/use-cases/create-chat-use-case';
 
 export function configureDependencies() {
-    //seu codigo aqui
+    const despesaRepository = new RepositoryData();
+    const createDespesaUseCase = new CreateDespesaUseCase(despesaRepository);
+    const listAllDespesasksUseCase = new GetDespesasByUserUseCase(despesaRepository);
+
+    const chatRepository = new RepositoryAI();
+    const createChatUseCase = new CreateChatUseCase(chatRepository, despesaRepository);
+
+    const despesaController = new DespesaController(createDespesaUseCase, listAllDespesasksUseCase);
+    const chatController = new ChatController(createChatUseCase);
+
+    return {
+        despesaController,
+        chatController
+    }
+
 } 
